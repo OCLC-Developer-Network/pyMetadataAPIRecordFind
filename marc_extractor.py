57dce353d6b114b7a7b16aefed7122cd67b18429
Karen Coombs <coombsk@oclc.org>
2025-12-15 13:55:17 -0500

    updated license info

--- marc_extractor.py
+++ marc_extractor.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+# Copyright 2024
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 MARC Data Extractor
 
@@ -12,9 +26,6 @@ Extracted fields:
 - 260$c or 264$c: Publication Date (normalized to 4-digit year)
 - 300: Physical Description (all subfields)
 - Format: Combined LDR 06 + 008 23 logic
-
-Author: AI Assistant
-License: Apache License 2.0
 """
 
 import argparse
